d4c411a7ae6ff9686266166122d0940e36f990f7
bishiguro <bgishiguro@gmail.com>
2014-10-17 13:55:27 -0400

    Radar

--- radar.py
+++ radar.py
@@ -9,6 +9,9 @@ class Radar (MobileThing):
 
     def use (self,actor):
         actor.say('I fiddle with the buttons on ' + self.name());
+        for room in Room.getRooms():
+        	for item in room.contents():
+        		disp('I detect ' + item.name + ' in ' + room.name)
+
         # FIX ME
         #actor.say("Mmm. It looks like it's broken...")
-        
